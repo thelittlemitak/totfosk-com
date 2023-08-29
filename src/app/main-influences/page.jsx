07a08e68b1e169b818e455d0306329be2ddcb450
Jose Riera Tur <rieraturjose@gmail.com>
2023-08-29 20:42:12 +0200

    create module out of quotes

--- src/app/main-influences/page.jsx
+++ src/app/main-influences/page.jsx
@@ -1,17 +1,14 @@
 import AlbumWrapper from "./AlbumWrapper";
 import styles from "./main-influences.module.css";
 import albums from "../../data/albums";
+import quotes from "../../data/quotes";
 
 export default function MainInfluencesPage() {
   let counter = 0;
 
   return (
     <>
-      <div className={styles.intro}>
-        &quot;There are some albums that change everything. You just hear them
-        in a constant loop for weeks, sometimes months, without hearing anything
-        else.&quot;
-      </div>
+      <div className={styles.intro}>{quotes[0]}</div>
       <div className={styles.pageWrapper}>
         {albums.slice(0, 9).map((x) => {
           counter++;
@@ -25,10 +22,7 @@ export default function MainInfluencesPage() {
           );
         })}
       </div>
-      <div className={styles.intro}>
-        &quot;I listen to them before going to sleep, with my eyes closed, the
-        style doesn&apos;t matter. Anything good takes my full attention.&quot;
-      </div>
+      <div className={styles.intro}>{quotes[1]}</div>
       <div className={styles.pageWrapper}>
         {albums.slice(10, 21).map((x) => {
           counter++;
@@ -42,11 +36,7 @@ export default function MainInfluencesPage() {
           );
         })}
       </div>
-      <div className={styles.intro}>
-        &quot;Your hearing develops with time, and you get deeper into more
-        complex styles (and together with it, deeper emotions).
-        <p>There&apos;s no way back.&quot;</p>
-      </div>
+      <div className={styles.intro}>{quotes[2]}</div>
       <div className={styles.pageWrapper}>
         {albums.slice(21, 31).map((x) => {
           counter++;
@@ -60,11 +50,7 @@ export default function MainInfluencesPage() {
           );
         })}
       </div>
-      <div className={styles.intro}>
-        &quot;Albums are not ranked here as better or worst, but regarding how
-        much time I spent with them or rather how much they affected me. And
-        this depends a lot on which time they were discovered.&quot;
-      </div>
+      <div className={styles.intro}>{quotes[3]}</div>
       <div className={styles.pageWrapper}>
         {albums.slice(31, 41).map((x) => {
           counter++;
@@ -79,10 +65,7 @@ export default function MainInfluencesPage() {
         })}
       </div>
       <div className={styles.intro}>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates
-        fuga ullam beatae accusamus corporis voluptatibus pariatur illo.
-        Mollitia incidunt enim quod laboriosam doloremque minus, modi nihil
-        atque aperiam omnis accusamus.
+        {quotes[4]}
       </div>
       <div className={styles.pageWrapper}>
         {albums.slice(41, 51).map((x) => {

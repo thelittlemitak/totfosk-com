6aaaf3084a7daaf0168a39ca499ae7e8ab474bae
Jose Riera Tur <rieraturjose@gmail.com>
2023-09-10 23:02:49 +0200

    add concerts data

---
+++ src/data/concerts.js
@@ -0,0 +1,38 @@
+const concerts = [
+  {
+    band: "Quartetto Minimo",
+    year: "2011",
+    place: "Ibiza",
+    experience: "bla bla bla",
+  },
+  {
+    band: "Antonio Sanchez & Bad Hombre",
+    year: "2021",
+    place: "Berlin",
+    experience: "bla bla bla",
+  },
+  {
+    band: "Tim Hecker",
+    year: "2023",
+    place: "Berlin",
+    experience: "bla bla bla",
+  },
+  {
+    band: "Bora Bora DJ",
+    year: "2006",
+    place: "Ibiza",
+    experience: "bla bla bla",
+  },
+  {
+    band: "Hybral",
+    year: "2022",
+    place: "Berlin",
+    experience: "bla bla bla",
+  },
+  {
+    band: "Hybral",
+    year: "2022",
+    place: "Berlin",
+    experience: "bla bla bla",
+  },
+];
